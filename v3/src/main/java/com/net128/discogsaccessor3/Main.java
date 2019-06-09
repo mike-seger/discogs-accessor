08469048e26681f26db61ba144877c5248eb6292
Michael Seger <michael@macbookpro>
2019-06-10 01:27:58 +0200

    added search example

--- v3/src/main/java/com/net128/discogsaccessor3/Main.java
+++ v3/src/main/java/com/net128/discogsaccessor3/Main.java
@@ -5,8 +5,7 @@ import org.slf4j.LoggerFactory;
 import ru.blizzed.discogsdb.DiscogsAuthData;
 import ru.blizzed.discogsdb.DiscogsDBApi;
 import ru.blizzed.discogsdb.DiscogsDBErrorException;
-import ru.blizzed.discogsdb.model.Page;
-import ru.blizzed.discogsdb.model.search.BaseSearchResult;
+import ru.blizzed.discogsdb.model.SearchPage;
 import ru.blizzed.discogsdb.params.DiscogsDBParams;
 
 import java.io.FileInputStream;
@@ -28,11 +27,28 @@ public class Main {
         DiscogsDBApi.initialize(new DiscogsAuthData(
             configProps.getProperty("consumer.key"),
             configProps.getProperty("consumer.secret")));
-        Page<BaseSearchResult> page = DiscogsDBApi.searchArtist(
-            DiscogsDBParams.QUERY.of("nostrum")
-        ).execute();
-        logger.info("Got: {} results", page.getContent().size());
-        long kornId = page.getContent().get(0).getId();
+        SP [] sps= new SP [] {
+            new SP("nostrum", "baby"),
+            new SP("nostrum", "monastery"),
+        };
+        for(SP sp : sps) {
+            SearchPage searchPage = DiscogsDBApi.search(
+                DiscogsDBParams.ARTIST.of(sp.artist),
+                DiscogsDBParams.TRACK.of(sp.title)
+            ).execute();
+            logger.info("Search: {} results", searchPage.getResults().size());
+            searchPage.getAllReleases().stream().forEach(r ->
+                System.out.println(r.getTitle()+" / "+r.getYear()+ " / "+r.getLabels()));
+        }
     }
 
+    class SP {
+        String artist;
+        String title;
+
+        public SP(String artist, String title) {
+            this.artist = artist;
+            this.title = title;
+        }
+    }
 }
